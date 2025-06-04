32cc69058d076bc0a044cfa92a4166ace3725135
Levon Aslanyan <aslanyan2012@mail.ru>
2025-06-04 19:30:34 +0400

    adding routes page

---
+++ client/src/routes/homePage/homePage.jsx
@@ -0,0 +1,7 @@
+import "./homePage.css";
+
+const HomePage = () => {
+  return <div>HomePage</div>;
+};
+
+export default HomePage;
---
+++ client/src/routes/postPage/postPage.jsx
@@ -0,0 +1,7 @@
+import "./postPage.css";
+
+const PostPage = () => {
+  return <div>PostPage</div>;
+};
+
+export default PostPage;
