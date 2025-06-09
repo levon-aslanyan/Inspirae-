12efaa4004291cf4bb4076f053eb668830744b95
Levon Aslanyan <aslanyan2012@mail.ru>
2025-06-09 19:55:30 +0400

    route and layout data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,12 @@
-import "./app.css";
-import Gallery from "./components/gallery/gallery";
-import LeftBar from "./components/leftBar/leftBar";
-import TopBar from "./components/topBar/topBar";
+// import "./app.css";
+// import Gallery from "./components/gallery/gallery";
+// import LeftBar from "./components/leftBar/leftBar";
+// import TopBar from "./components/topBar/topBar";
 
-const App = () => {
-  return (
-    <div className="app">
-      <LeftBar></LeftBar>
+// const App = () => {
+//   return (
+    
+//   );
+// };
 
-      <div className="content">
-        <TopBar></TopBar>
-        <Gallery></Gallery>
-      </div>
-    </div>
-  );
-};
-
-export default App;
+// export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,52 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import HomePage from "./routes/homePage/homePage.jsx";
+import CreatePage from "./routes/createPage/createPage.jsx";
+import PostPage from "./routes/postPage/postPage.jsx";
+import AuthPage from "./routes/authPage/authPage.jsx";
+import SearchPage from "./routes/searchPage/searchPage.jsx";
+import ProfilePage from "./routes/profilePage/profilePage.jsx";
+import MainLayout from "./routes/layout/mainLayout.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "/create",
+        element: <CreatePage />,
+      },
+      {
+        path: "/pin/:id",
+        element: <PostPage />,
+      },
+
+      {
+        path: "/search",
+        element: <SearchPage />,
+      },
+      {
+        path: "/:username",
+        element: <ProfilePage />,
+      },
+    ],
+  },
+
+  {
+    path: "/auth",
+    element: <AuthPage />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router}></RouterProvider>
+  </StrictMode>
+);
