faf85d4bac4a0e576345fa5348c8d20857c7fbbf
Levon Aslanyan <aslanyan2012@mail.ru>
2025-08-16 23:53:46 +0400

    profile page

---
+++ client/src/components/collections/collections.jsx
@@ -0,0 +1,60 @@
+import "./collections.css";
+import ItemImage from "../../components/itemImage/ItemImage";
+
+const Collections = () => {
+  return (
+    <div className="collections">
+      <div className="collection">
+        <ItemImage path="pins/pin1.jpeg" alt="" />
+        <div className="collectionInfo">
+          <h1>Minimalis Bedrooms</h1>
+          <span>12pin / 1w</span>
+        </div>
+      </div>
+      <div className="collection">
+        <ItemImage path="pins/pin1.jpeg" alt="" />
+        <div className="collectionInfo">
+          <h1>Minimalis Bedrooms</h1>
+          <span>12pin / 1w</span>
+        </div>
+      </div>
+      <div className="collection">
+        <ItemImage path="pins/pin1.jpeg" alt="" />
+        <div className="collectionInfo">
+          <h1>Minimalis Bedrooms</h1>
+          <span>12pin / 1w</span>
+        </div>
+      </div>
+      <div className="collection">
+        <ItemImage path="pins/pin1.jpeg" alt="" />
+        <div className="collectionInfo">
+          <h1>Minimalis Bedrooms</h1>
+          <span>12pin / 1w</span>
+        </div>
+      </div>
+      <div className="collection">
+        <ItemImage path="pins/pin1.jpeg" alt="" />
+        <div className="collectionInfo">
+          <h1>Minimalis Bedrooms</h1>
+          <span>12pin / 1w</span>
+        </div>
+      </div>
+      <div className="collection">
+        <ItemImage path="pins/pin1.jpeg" alt="" />
+        <div className="collectionInfo">
+          <h1>Minimalis Bedrooms</h1>
+          <span>12pin / 1w</span>
+        </div>
+      </div>
+      <div className="collection">
+        <ItemImage path="pins/pin1.jpeg" alt="" />
+        <div className="collectionInfo">
+          <h1>Minimalis Bedrooms</h1>
+          <span>12pin / 1w</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Collections;
